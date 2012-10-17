408bcb2a31462b098ab884c1e14a98212c3f7222
Rafal Groszek <rgroszek@sigma.ug.edu.pl>
2012-10-17 11:33:51 +0200

    animal

--- ruby.rb
+++ ruby.rb
@@ -3,3 +3,24 @@ class Animal
     @health = 0
   end
 end
+
+class Fox < Animal
+  attr_accessor :health
+  def self.breeds
+    ['snow fox', 'desert fox']
+  end
+  def initialize
+    super
+    @health += 5
+  end
+  def eat(food)
+    if likes_food?(food)
+      @health += 5
+    else
+      @health += 1
+    end
+  end
+  def bark
+    puts 'wrrrr' if @health > 0
+    @health -= 1
+  end
